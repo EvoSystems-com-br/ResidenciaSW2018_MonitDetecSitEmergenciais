4ab3b8c754710989220ee952228dda95f88e8840
LeonardoCRamos <leo_ramos_95@hotmail.com>
2018-08-23 11:35:54 -0300

    Update GPS_location.ino

--- Código-fonte/GPS_location.ino
+++ Código-fonte/GPS_location.ino
@@ -7,26 +7,30 @@
 char MESSAGE[300];
 char lat[12];
 char lon[12];
+bool testeSMS = false;
+bool teste = true;
 
 //Comunicacao serial com o SIM808
 //PIN RX 16
 //PIN TX 17
 HardwareSerial Serial2(2);
 
+SIM808_methods class_sim808(&Serial2);
+
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600);
 
   //Iniciando comunicacao com o SIM808
-  while(!sim808_AT()) {
+  while(!(class_sim808.init())) {
     delay(1000);
-    Serial.print("Sem comunicacao\r\n");
+    Serial.print("\r\nSem comunicacao\r\n");
   }
-  Serial.println("SIM808 conectado!");
+  Serial.println("\r\nSIM808 conectado!");
   Serial.println("Ligando GPS...");
 
   //Ligando GPS
-  while(!sim808_turnon_GPS()) {
+  while(!(class_sim808.turnon_GPS())) {
     delay(1000);
     Serial.print("Problema ao ligar GPS...\r\n");
   }
@@ -34,43 +38,66 @@ void setup() {
 }
 
 void loop() {
-  
-  //Ler dados GPS do SIM808
-  if(sim808_gotGPS()) {
-    Serial.print(sim808.GPSdata.year);
-    Serial.print("/");
-    Serial.print(sim808.GPSdata.month);
-    Serial.print("/");
-    Serial.print(sim808.GPSdata.day);
-    Serial.print(" ");
-    Serial.print(sim808.GPSdata.hour);
-    Serial.print(":");
-    Serial.print(sim808.GPSdata.minute);
-    Serial.print(":");
-    Serial.print(sim808.GPSdata.second);
-    Serial.print(":");
-    Serial.println(sim808.GPSdata.centisecond);
-    Serial.print("latitude :");
-    Serial.println(sim808.GPSdata.lat);
-    Serial.print("longitude :");
-    Serial.println(sim808.GPSdata.lon);
-    Serial.print("speed_kph :");
-    Serial.println(sim808.GPSdata.speed_kph);
-    Serial.print("heading :");
-    Serial.println(sim808.GPSdata.heading);
-    Serial.println();
-    
-    Serial.println("Mandando mensagem para %s com conteudo:\n", PHONE);
-    sprintf(MESSAGE, "Latitude : %s\nLongitude : %s\n", lat, lon);
+  /*if (Serial2.available()) {
+    Serial.write(Serial2.read());
+  }
+  if (Serial.available()) {
+    while(Serial.available()) {
+      Serial2.write(Serial.read());
+    }
+  }*/
 
-    //Enviando mensagem
-    sim808_sendSMS(PHONE,MESSAGE);
+  if(testeSMS) {
+    if(class_sim808.send_SMS(PHONE,"teste"))
+      Serial.println("Sucesso");
 
     //Desligando GPS
-    while(!sim808_turnoff_GPS()) {
+    delay(500);
+    while(!(class_sim808.turnoff_GPS())) {
+      delay(1000);
+      Serial.print("Problema ao desligar GPS...\r\n");
+    }
+    Serial.println("GPS desligado com sucesso!");
+    testeSMS = false;
+  }
+   
+  //Ler dados GPS do SIM808
+  if((class_sim808.GPS_status())&&(teste)) {
+
+    class_sim808.read_GPS();
+
+    Serial.print("\n utc :");
+    Serial.println(class_sim808.GPSdata.utc);
+    Serial.print("latitude :");
+    Serial.println(class_sim808.GPSdata.lat);
+    Serial.print("longitude :");
+    Serial.println(class_sim808.GPSdata.lon);
+    Serial.print("altitude :");
+    Serial.println(class_sim808.GPSdata.alt);
+    Serial.print("speed_knots :");
+    Serial.println(class_sim808.GPSdata.speed_knots);
+    Serial.print("heading :");
+    Serial.println(class_sim808.GPSdata.heading);
+
+    float la = atof(class_sim808.GPSdata.lat);
+    Serial.println(la);
+    dtostrf(la, 4, 6, lat); //put float value of la into char array of lat. 4 = number of digits before decimal sign. 6 = number of digits after the decimal sign.
+    
+    Serial.print("Mandando mensagem para ");
+    Serial.print(PHONE);
+    Serial.println(" com conteudo:\n");
+    sprintf(MESSAGE, lat);
+
+    //Enviando mensagem
+    class_sim808.send_SMS(PHONE,MESSAGE);
+
+    //Desligando GPS
+    delay(500);
+    while(!(class_sim808.turnoff_GPS())) {
       delay(1000);
       Serial.print("Problema ao desligar GPS...\r\n");
     }
     Serial.println("GPS desligado com sucesso!");
   }
+  delay(1000);
 }
